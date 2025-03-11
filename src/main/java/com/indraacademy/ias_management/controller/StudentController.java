61f9aa05dd3dc0120c906567de7b62942d62a106
Bhumit Joshi <bhumitjoshi2003@gmail.com>
2025-03-11 16:06:15 +0530

    done with the keycloaok configurations

--- src/main/java/com/indraacademy/ias_management/controller/StudentController.java
+++ src/main/java/com/indraacademy/ias_management/controller/StudentController.java
@@ -2,20 +2,22 @@ package com.indraacademy.ias_management.controller;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/student")
+@RequestMapping("")
+@CrossOrigin(origins = "http://localhost:4200")
 public class StudentController {
 
-    @GetMapping("/detail")
-    public String getStudentDetails() {
+    @GetMapping("/message")
+    public String getStudentMessage() {
         return "HARIBOL, I am a student.";
     }
 
-    @GetMapping("/details")
+    @GetMapping("/student/details")
     public String getStudentDetails(@AuthenticationPrincipal Jwt jwt) {
         return "Student: " + jwt.getClaimAsString("preferred_username");
     }
