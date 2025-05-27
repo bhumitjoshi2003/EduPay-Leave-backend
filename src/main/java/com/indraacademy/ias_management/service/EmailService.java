d1aeb92ba64eb15f0f579141ea1fd92e57745616
Bhumit Joshi <bhumitjoshi2003@gmail.com>
2025-05-27 12:39:55 +0530

    Fee confirmation Email after successful payment

--- src/main/java/com/indraacademy/ias_management/service/EmailService.java
+++ src/main/java/com/indraacademy/ias_management/service/EmailService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,6 +21,7 @@ public class EmailService {
     @Value("${spring.mail.username}")
     private String emailSender;
 
+    @Async
     public void sendEmail(String to, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom(emailSender);
