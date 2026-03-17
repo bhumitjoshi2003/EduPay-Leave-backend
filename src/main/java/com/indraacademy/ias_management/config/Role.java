61fe57c349a53113a2e8faff9c9a7eec9fef643a
Bhumit Joshi <bhumitjoshi2003@gmail.com>
2026-03-17 11:59:06 +0530

    Added Finance_Admin role for fees realted operations

--- src/main/java/com/indraacademy/ias_management/config/Role.java
+++ src/main/java/com/indraacademy/ias_management/config/Role.java
@@ -4,6 +4,7 @@ public class Role {
     public static final String ADMIN = "ADMIN";
     public static final String TEACHER = "TEACHER";
     public static final String STUDENT = "STUDENT";
+    public static final String FINANCE_ADMIN = "FINANCE_ADMIN";
 
     private Role() {}
 }
