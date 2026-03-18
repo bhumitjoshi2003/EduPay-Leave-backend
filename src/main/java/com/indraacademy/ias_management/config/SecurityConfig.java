e578c0fe3069857dfb7992e24cb114bb560895cd
Bhumit Joshi <bhumitjoshi2003@gmail.com>
2026-03-18 16:21:37 +0530

    Temporary allowing all the request

--- src/main/java/com/indraacademy/ias_management/config/SecurityConfig.java
+++ src/main/java/com/indraacademy/ias_management/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
         http
                 .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
-                    config.setAllowedOrigins(List.of(frontendUrl));
+                    config.setAllowedOrigins(List.of("*"));
                     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     config.setAllowedHeaders(List.of("*"));
                     config.setAllowCredentials(true);
